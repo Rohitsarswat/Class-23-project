48341ea048afcc105ac5367f22bd41d9fea748e7
Rohitsarswat <83912817+Rohitsarswat@users.noreply.github.com>
2021-11-15 12:23:34 +0530

    Update PlayerArrow.js

--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -14,7 +14,25 @@ class PlayerArrow {
   }
 
   display() {
-  
+  var tmpAngle;
+    if (this.body.velocity.y === 0) {
+      tmpAngle = this.archerAngle + 90;
+    } else {
+      tmpAngle =
+        Math.atan(this.body.velocity.y / this.body.velocity.x) * (180 / 3.14);
+    }
+
+    Matter.Body.setAngle(this.body, tmpAngle);
+
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
    
   }
 }
